5d80278698b6216893ef513b994a1b48d92b0d97
Alexandre Halas <alexandrehalas@gmail.com>
2023-09-28 20:54:49 -0300

    AJAX Call: XMLHttpRequest

--- script.js
+++ script.js
@@ -4,3 +4,35 @@ const btn = document.querySelector('.btn-country');
 const countriesContainer = document.querySelector('.countries');
 
 ///////////////////////////////////////
+
+// old school way XMLHttpRequest
+
+const getCountry = function (name) {
+  const request = new XMLHttpRequest();
+  request.open('GET', `https://restcountries.com/v3.1/name/${name}`);
+  request.send();
+
+  request.addEventListener('load', function () {
+    const [country] = JSON.parse(this.responseText);
+    const html = `
+    <article class="country">
+        <img class="country__img" src="${country.flags.svg}" />
+        <div class="country__data">
+        <h3 class="country__name">${country.name.common}</h3>
+        <h4 class="country__region">${country.region}</h4>
+        <p class="country__row"><span>👫</span>${(
+          +country.population / 1_000_000
+        ).toFixed(1)}</p>
+        <p class="country__row"><span>🗣️</span>${
+          Object.values(country.languages)[0]
+        }</p>
+        <p class="country__row"><span>💰</span>${
+          Object.values(country.currencies)[0].name
+        }</p>
+        </div>
+    </article>`;
+
+    countriesContainer.insertAdjacentHTML('beforeend', html);
+    countriesContainer.style.opacity = 1;
+  });
+};
