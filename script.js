5faba11bc2a27112c46de36fca9bb72e6c4a749a
Alexandre Halas <alexandrehalas@gmail.com>
2023-10-16 18:11:31 -0300

    Promisifying the Geolocation API

--- script.js
+++ script.js
@@ -125,6 +125,7 @@ console.log('second, because is callback queue', 'Test end');
 //     console.error(error);
 //   });
 
+/*
 // Promisifying setTimeout
 const wait = function (seconds) {
   return new Promise(function (resolve) {
@@ -167,3 +168,45 @@ wait(1)
 // resolve or reject immediately
 Promise.resolve('abc').then(res => console.log(res));
 Promise.reject(new Error('abc')).catch(err => console.error(err));
+*/
+
+// PROMISIFYING THE GEOLOCATION API
+
+const getPosition = function () {
+  return new Promise(function (resolve, reject) {
+    // navigator.geolocation.getCurrentPosition(
+    //   position => resolve(position),
+    //   err => reject(err)
+    // );
+    navigator.geolocation.getCurrentPosition(resolve, reject);
+  });
+};
+// getPosition().then(pos => console.log(pos));
+
+const whereAmI = function () {
+  getPosition()
+    .then(pos => {
+      const { latitude: lat, longitude: lng } = pos.coords;
+
+      return fetch(`https://geocode.xyz/${lat},${lng}?geoit=json`);
+    })
+    .then(res => {
+      if (!res.ok) throw new Error(`Problem with geocoding ${res.status}`);
+      return res.json();
+    })
+    .then(data => {
+      console.log(data);
+      console.log(`You are in ${data.city}, ${data.country}`);
+
+      return fetch(`https://restcountries.eu/rest/v2/name/${data.country}`);
+    })
+    .then(res => {
+      if (!res.ok) throw new Error(`Country not found (${res.status})`);
+
+      return res.json();
+    })
+    .then(data => renderCountry(data[0]))
+    .catch(err => console.error(`${err.message} 💥`));
+};
+
+btn.addEventListener('click', whereAmI);
